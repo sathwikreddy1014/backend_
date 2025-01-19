853440ce82c0914bf9cdcc22f0004557d3e0291b
sathwikreddy1014 <sathwik1014@gmail.com>
2025-01-19 14:40:35 +0530

    connected DB

---
+++ src/index.js
@@ -0,0 +1,35 @@
+ import dotenv from "dotenv"
+
+// import mongoose from "mongoose";
+// import {DB_NAME} from "./constants";
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path: "./env"
+})
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ( async () => {
+//     try {
+//         await mongoose.connect(`${process,env.MONGODB_URI}/${DB_NAME}`)
+//     } catch (error) { 
+//         console.log("ERROR", error);
+//         throw err
+//     }
+// })()
